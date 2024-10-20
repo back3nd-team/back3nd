e9921007a7ab855572e22cdba56f786abd5fd05e
hermesalvesbr <hermes.alves@araripina.com.br>
2024-10-20 09:37:50 -0300

    chore: rows counting in a table

--- packages/studio/app/pages/admin/users/index.vue
+++ packages/studio/app/pages/admin/users/index.vue
@@ -12,6 +12,8 @@ const columns = [
   { label: 'Email', key: 'email', class: 'hidden md:table-cell', rowClass: 'hidden md:table-cell' },
   { label: 'Role', key: 'role' },
 ]
+const selectedColumns = ref([...columns])
+
 const people = [{
   id: 1,
   name: 'Lindsay Walton',
@@ -49,6 +51,7 @@ const people = [{
   email: 'floyd.miles@example.com',
   role: 'Member',
 }]
+
 const q = ref('')
 
 const filteredRows = computed(() => {
@@ -62,14 +65,92 @@ const filteredRows = computed(() => {
     })
   })
 })
+
+const isOpen = ref(false)
+function clearForm() {
+  console.warn('clear form')
+}
+
+function saveItem() {
+  console.warn('save item')
+}
+
+function select(row) {
+  const index = selected.value.findIndex(item => item.id === row.id)
+  if (index === -1) {
+    selected.value.push(row)
+  }
+  else {
+    selected.value.splice(index, 1)
+  }
+}
+
+const selected = ref([])
 </script>
 
 <template>
   <div>
-    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
-      <UInput v-model="q" placeholder="Filter people..." />
+    <div class="flex justify-between items-center px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <div class="flex space-x-4">
+        <UInput v-model="q" placeholder="Filter people..." />
+        <USelectMenu v-model="selectedColumns" :options="columns" multiple placeholder="Columns" />
+      </div>
+      <div id="actions-buttons">
+        <UButton
+          icon="lets-icons:add-ring-fill"
+          size="sm"
+          color="primary"
+          square
+          variant="solid"
+          @click="isOpen = true"
+        />
+      </div>
     </div>
+    <UTable v-model="selected" :rows="filteredRows" :columns="selectedColumns" @select="select">
+      <template #caption>
+        <caption id="rows" class="text-xs text-gray-500 text-right pr-4 py-3">
+          125 rows in back3nd_users
+        </caption>
+      </template>
+      <template #empty-state>
+        <div class="flex flex-col items-center justify-center py-6 gap-3">
+          <span class="italic text-sm">No one here!</span>
+          <UButton label="Add people" />
+        </div>
+      </template>
+    </UTable>
+    <USlideover :model-value="isOpen" @update:model-value="isOpen = false">
+      <UCard class="flex flex-col h-full">
+        <template #header>
+          <div class="flex justify-between items-center">
+            <h3 class="text-lg font-medium">
+              Add New Item
+            </h3>
+            <UButton
+              id="close-button"
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="isOpen = false"
+            />
+          </div>
+        </template>
 
-    <UTable :rows="filteredRows" :columns="columns" />
+        <div class="p-4 space-y-4">
+          <!-- Inputs do formulário -->
+          <UInput label="Name" placeholder="Enter name" />
+          <UInput label="Email" placeholder="Enter email" />
+          <UInput label="Role" placeholder="Enter role" />
+        </div>
+
+        <template #footer>
+          <div class="flex justify-end space-x-4 p-4">
+            <UButton label="Clear" color="gray" @click="clearForm" />
+            <UButton label="Save" color="primary" @click="saveItem" />
+          </div>
+        </template>
+      </UCard>
+    </USlideover>
   </div>
 </template>
