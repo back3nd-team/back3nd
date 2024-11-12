a5b17edbe6db835979a0fd6dc9f702095ed29d54
hermesalvesbr <hermes.alves@araripina.com.br>
2024-11-12 07:54:14 -0300

    fix: corrigir URL do link da documentação da API removendo barra antes do endpoint

--- packages/studio/app/components/layout/MenuItems.vue
+++ packages/studio/app/components/layout/MenuItems.vue
@@ -20,7 +20,7 @@ const apiBase = useApiClient.getBaseURL()
       </NuxtLink>
     </li>
     <li>
-      <NuxtLink :to="`${apiBase}/docs/swagger`" target="_blank" class="block py-3 px-5 rounded bg-gray-700 hover:bg-yellow-500 transition-all duration-300">
+      <NuxtLink :to="`${apiBase}docs/swagger`" target="_blank" class="block py-3 px-5 rounded bg-gray-700 hover:bg-yellow-500 transition-all duration-300">
         <i class="fas fa-table mr-2" /> API Doc´s
       </NuxtLink>
     </li>
