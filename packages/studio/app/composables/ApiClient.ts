03d76e270bded2af484a42991c8222de019be291
hermesalvesbr <hermes.alves@araripina.com.br>
2024-11-12 06:56:38 -0300

    fix: adicionar log para exibir URL completa no método de login

--- packages/studio/app/composables/ApiClient.ts
+++ packages/studio/app/composables/ApiClient.ts
@@ -63,6 +63,7 @@ class ApiClient {
 
   public async login(email: string, password: string): Promise<void> {
     const urlSanity = `${this.baseURL}auth/login`
+    console.log('URL COMPLETA', urlSanity, this.baseURL)
     const response = await fetch(urlSanity, {
       method: 'POST',
       body: JSON.stringify({ email, password }),
